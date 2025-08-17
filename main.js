12716b11f89e42620e1e452eadfe5938a43f923b
danny-mendoza1 <d.mendoza875@gmail.com>
2025-08-17 13:58:29 -0500

    adds pdf lib code directly to project, verified library works

--- main.js
+++ main.js
@@ -1,12 +1,55 @@
-    const button = document.querySelector("#toggle")
+const button = document.querySelector("#toggle");
+const dropZone = document.querySelector("#drop-zone");
+const inputFile = document.querySelector("#input-file");
 
-    button.addEventListener("click", (event) => {
-        const text = document.querySelector("#mainText")
-        console.log(text.innerHTML)
-        if (text.innerHTML === "Hello World") {
-            text.innerHTML = "Hello Universe"
-        }
-        else {
-            text.innerHTML = "Hello World"
-        }
-    })
+
+// button.addEventListener("click", (event) => {
+//   const text = document.querySelector("#mainText");
+//   console.log(text.innerHTML);
+//   if (text.innerHTML === "Hello World") {
+//     text.innerHTML = "Hello Universe";
+//   } else {
+//     text.innerHTML = "Hello World";
+//   }-
+// });
+
+dropZone.addEventListener("drop", dropHandler)
+dropZone.addEventListener("dragover", dragOverHandler)
+inputFile.addEventListener("change", handleFileInput )
+
+function dropHandler(event){
+    console.log("File dropped", event);
+    event.preventDefault();
+    // handleFiles(event.dataTransfer.files)
+}
+
+function dragOverHandler(event){
+    console.log("file in dropzone")
+    event.preventDefault();
+}
+
+async function handleFiles(fileList){
+    const files = Array.from(fileList)
+
+    const buffers = await Promise.all(files.map(file => file.arrayBuffer()));
+    const bytes = buffers[0]
+    console.log(bytes)
+
+    const pdfDoc = await PDFLib.PDFDocument.load(bytes)
+    const pages = pdfDoc.getPageCount()
+    console.log(pages)
+
+    //load pdf to pdf-lib
+}
+
+async function handleFileInput(event){
+
+     const files = Array.from(event.target.files)
+
+    const buffers = await Promise.all(files.map(file => file.arrayBuffer()));
+    const bytes = buffers[0]
+
+    const pdfDoc = await PDFLib.PDFDocument.load(bytes)
+    const pages = pdfDoc.getPageCount()
+    console.log(pages)
+}
