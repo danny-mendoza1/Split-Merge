bc38bcea3794924b90e469b291c9f74de2992d7b
danny-mendoza1 <d.mendoza875@gmail.com>
2025-08-17 16:38:43 -0500

    clean up

--- main.js
+++ main.js
@@ -2,53 +2,25 @@ const button = document.querySelector("#toggle");
 const dropZone = document.querySelector("#drop-zone");
 const inputFile = document.querySelector("#input-file");
 
+dropZone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+});
 
-// button.addEventListener("click", (event) => {
-//   const text = document.querySelector("#mainText");
-//   console.log(text.innerHTML);
-//   if (text.innerHTML === "Hello World") {
-//     text.innerHTML = "Hello Universe";
-//   } else {
-//     text.innerHTML = "Hello World";
-//   }-
-// });
+dropZone.addEventListener("drop", async (e) => {
+    e.preventDefault()
+    const files = [...e.dataTransfer.files]
+    await handleFiles(files)
+})
 
-dropZone.addEventListener("drop", dropHandler)
-dropZone.addEventListener("dragover", dragOverHandler)
-inputFile.addEventListener("change", handleFileInput )
+inputFile.addEventListener("change", async(e) => {
+    const files = [...e.currentTarget.files];
+    await handleFiles(files)
+})
 
-function dropHandler(event){
-    console.log("File dropped", event);
-    event.preventDefault();
-    // handleFiles(event.dataTransfer.files)
-}
 
-function dragOverHandler(event){
-    console.log("file in dropzone")
-    event.preventDefault();
-}
-
-async function handleFiles(fileList){
-    const files = Array.from(fileList)
-
+async function handleFiles(files){
     const buffers = await Promise.all(files.map(file => file.arrayBuffer()));
     const bytes = buffers[0]
-    console.log(bytes)
-
-    const pdfDoc = await PDFLib.PDFDocument.load(bytes)
-    const pages = pdfDoc.getPageCount()
-    console.log(pages)
-
-    //load pdf to pdf-lib
-}
-
-async function handleFileInput(event){
-
-     const files = Array.from(event.target.files)
-
-    const buffers = await Promise.all(files.map(file => file.arrayBuffer()));
-    const bytes = buffers[0]
-
     const pdfDoc = await PDFLib.PDFDocument.load(bytes)
     const pages = pdfDoc.getPageCount()
     console.log(pages)
